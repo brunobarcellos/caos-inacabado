83b79efbc64af75539c9a2b0e0969e5ae3428ba8
Bruno Barcellos <bbarcellosf@gmail.com>
2021-04-06 17:01:12 -0300

    feat(padraoDAO): Adiciona padrao DAO
    - Passa a gerar a lista de livros de maneira dinâmica, utilizando padrão
    DAO para conexão com o banco
    
    BREAKING CHANGE: Padrão DAO

--- nodejs/fundamentos/src/app/rotas/rotas.js
+++ nodejs/fundamentos/src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function(req, resp) {
         resp.send(
@@ -15,21 +18,18 @@ module.exports = (app) => {
     });
     
     app.get('/livros', function(req, resp) {
-        resp.marko(
-            require('../views/livros/lista.marko'),
-            {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node avançado'
-                    }
-                ]
-            }
-        );
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.lista(function(erro, resultados) {        
+            resp.marko(
+                require('../views/livros/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+
+        });
     });
 };
 
