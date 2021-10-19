8b2f95221c45b17de96392a949b748aa459e4e7e
Bruno Barcellos <bbarcellsf@gmail.com>
2021-10-19 10:35:33 -0300

    feat(Impractical Python Projects): Adiciona leitura de novo livro de python

---
+++ livros/python/impractical-projects/pig_latin_practice.py
@@ -0,0 +1,3 @@
+"""To form Pig Latin, you take an English word that begins with a consonant, move that consonant to the end, and then add “ay” to the end of the word. If the word begins with a vowel, you simply add “way” to the end of the word. One of the most famous Pig Latin phrases of all time is “ixnay on the ottenray,” uttered by Marty Feldman in Mel Brooks’s comedic masterpiece Young Frankenstein.
+
+Write a program that takes a word as input and uses indexing and slicing to return its Pig Latin equivalent. """
